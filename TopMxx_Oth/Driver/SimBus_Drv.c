7c4a2cb91365f7ab2635cea344467f4e6a37a9ab
Jayus <1469829831@qq.com>
2026-09-17 16:28:37 +0800

    SB移植成功

--- TopMxx_Oth/Driver/SimBus_Drv.c
+++ TopMxx_Oth/Driver/SimBus_Drv.c
@@ -37,20 +37,20 @@
 
 #include "Include.h"
 /*======================================== 模块内有效宏定义 =======================================*/
-#define	SB_SetRd()		GPIO_BOP(GPIOE)	=mBit14							//置Rd信号
-#define SB_ClrRd()		GPIO_BC(GPIOE)	=mBit14							//清Rd信号
-#define SB_SetWr()		GPIO_BOP(GPIOE)	=mBit15							//置Wr信号
-#define SB_ClrWr()		GPIO_BC(GPIOE)	=mBit15							//清Wr信号
-#define	SB_SetDir()		//GPIO_BOP(GPIOB)	=mBit0							//置Dir信号，数据方向设置为输出
-#define SB_ClrDir()		//GPIO_BC(GPIOB)	=mBit0							//清Dir信号，数据方向设置为输入
-#define	SB_SetRst()		GPIO_BOP(GPIOD)	=mBit15							//置Rst信号，复位释放，总线使能
-#define SB_ClrRst()		GPIO_BC(GPIOD)	=mBit15							//清Rst信号，复位数据总线
-#define SB_SetAddr(a)	GPIO_BOP(GPIOE)	=((((a)&0x003f)<<8)|((((~(a))&0x003f))<<(8+16)))		//设地址
-//数据总线   
-#define SB_SetDataRdDir()	GPIO_CTL(GPIOE)	&=(~(0x0000ffff))					//数据读方向
-#define SB_SetDataWrDir()	GPIO_CTL(GPIOE)	|=(0x00005555)						//数据写方向
-#define SB_SetData(d)		GPIO_BOP(GPIOE)	=(((d)&0xff)|(((~(d))&0xff)<<16))	//设数据	
-#define SB_GetData()		(GPIO_ISTAT(GPIOE)&0xff)							//获取数据
+#define	SB_SetRd()		GPIOE->BSRR	=mBit14									//置Rd信号
+#define SB_ClrRd()		GPIOE->BSRR	=((U32)mBit14<<16)						//清Rd信号
+#define SB_SetWr()		GPIOE->BSRR	=mBit15									//置Wr信号
+#define SB_ClrWr()		GPIOE->BSRR	=((U32)mBit15<<16)						//清Wr信号
+#define	SB_SetDir()		//GPIOB->BSRR	=mBit0								//置Dir信号，数据方向设置为输出（新硬件无SN74LVC16245，不使用）
+#define SB_ClrDir()		//GPIOB->BSRR	=((U32)mBit0<<16)					//清Dir信号，数据方向设置为输入（同上）
+#define	SB_SetRst()		GPIOD->BSRR	=mBit15									//置Rst信号，复位释放，总线使能
+#define SB_ClrRst()		GPIOD->BSRR	=((U32)mBit15<<16)						//清Rst信号，复位数据总线
+#define SB_SetAddr(a)	GPIOE->BSRR	=(((((a)&0x003f)<<8)|((((~(a))&0x003f))<<(8+16))))		//设地址
+//数据总线
+#define SB_SetDataRdDir()	GPIOE->MODER	&=(~(0x0000ffff))					//数据读方向
+#define SB_SetDataWrDir()	GPIOE->MODER	|=(0x00005555)						//数据写方向
+#define SB_SetData(d)		GPIOE->BSRR	=(((d)&0xff)|(((~(d))&0xff)<<16))		//设数据
+#define SB_GetData()		(GPIOE->IDR&0xff)								//获取数据
 
 
 /*======================================= 模块内有效变量定义 ======================================*/
@@ -81,39 +81,40 @@ int main(void)
 其    它：
 1、地址线A0～A5				PE8~PE13
    数据线D0～D7				PE0~PE7
-   写信号WR						PE15
-   读信号RD						PE14
-   CPLD总线复位RST 		PD15
-	 读写方向切换DDIR 	PB0
+   写信号WR					PE15
+   读信号RD					PE14
+   CPLD总线复位RST 			PD15
 ****************************************************************************************************/
 void SimBus_Drv_Init(void)
 {
-	// enable the LEDs GPIO clock 
-    //rcu_periph_clock_enable(RCU_GPIOB);
-    rcu_periph_clock_enable(RCU_GPIOD);
-    rcu_periph_clock_enable(RCU_GPIOE);
-  
+	LL_GPIO_InitTypeDef GPIO_InitStruct = {0};
+
+	// 使能GPIO时钟
+    LL_AHB4_GRP1_EnableClock(LL_AHB4_GRP1_PERIPH_GPIOD);
+    LL_AHB4_GRP1_EnableClock(LL_AHB4_GRP1_PERIPH_GPIOE);
+
 	SB_ClrRst();			//复位总线
 	SB_SetWr();				//复位Wr
 	SB_SetRd();				//复位Rd
 	SB_SetDir();       //复位Dir
 	SB_SetAddr(0);		//地址输出为0x00
-	
-	//数据线D0～D7		PE0~PE7
-    gpio_mode_set(GPIOE, GPIO_MODE_OUTPUT, GPIO_PUPD_PULLUP, mBit0|mBit1|mBit2|mBit3|mBit4|mBit5|mBit6|mBit7);
-    gpio_output_options_set(GPIOE, GPIO_OTYPE_PP, GPIO_OSPEED_50MHZ,mBit0|mBit1|mBit2|mBit3|mBit4|mBit5|mBit6|mBit7);
+
+	//数据线D0～D7		PE0~PE7	推挽输出，上拉，高速
+	GPIO_InitStruct.Pin = LL_GPIO_PIN_0|LL_GPIO_PIN_1|LL_GPIO_PIN_2|LL_GPIO_PIN_3|LL_GPIO_PIN_4|LL_GPIO_PIN_5|LL_GPIO_PIN_6|LL_GPIO_PIN_7;
+	GPIO_InitStruct.Mode = LL_GPIO_MODE_OUTPUT;
+	GPIO_InitStruct.Speed = LL_GPIO_SPEED_FREQ_VERY_HIGH;
+	GPIO_InitStruct.OutputType = LL_GPIO_OUTPUT_PUSHPULL;
+	GPIO_InitStruct.Pull = LL_GPIO_PULL_UP;
+	LL_GPIO_Init(GPIOE, &GPIO_InitStruct);
 	//设置为输出：地址线A0～A5	PE8~PE13		PE14~PE15读写控制线
-	gpio_mode_set(GPIOE, GPIO_MODE_OUTPUT, GPIO_PUPD_PULLUP, mBit8|mBit9|mBit10|mBit11|mBit12|mBit13|mBit14|mBit15);
-    gpio_output_options_set(GPIOE, GPIO_OTYPE_PP, GPIO_OSPEED_50MHZ,mBit8|mBit9|mBit10|mBit11|mBit12|mBit13|mBit14|mBit15);
+	GPIO_InitStruct.Pin = LL_GPIO_PIN_8|LL_GPIO_PIN_9|LL_GPIO_PIN_10|LL_GPIO_PIN_11|LL_GPIO_PIN_12|LL_GPIO_PIN_13|LL_GPIO_PIN_14|LL_GPIO_PIN_15;
+	LL_GPIO_Init(GPIOE, &GPIO_InitStruct);
     //设置为输出：CPLD_RST PD15
-	gpio_mode_set(GPIOD, GPIO_MODE_OUTPUT, GPIO_PUPD_PULLUP, mBit15);
-    gpio_output_options_set(GPIOD, GPIO_OTYPE_PP, GPIO_OSPEED_50MHZ,mBit15);
-	//SN74LVC16245读写方向切换 PB0	设置值为输出
-	//gpio_mode_set(GPIOB, GPIO_MODE_OUTPUT, GPIO_PUPD_PULLUP, mBit0);
-    //gpio_output_options_set(GPIOB, GPIO_OTYPE_PP, GPIO_OSPEED_50MHZ,mBit0);
-    
+	GPIO_InitStruct.Pin = LL_GPIO_PIN_15;
+	LL_GPIO_Init(GPIOD, &GPIO_InitStruct);
+
 	SB_SetRst();		//总线使能
-	SB_SetAddr(0);		//地址输出为0x00	
+	SB_SetAddr(0);		//地址输出为0x00
 }
 
 /****************************************************************************************************
@@ -127,7 +128,7 @@ U8 SB_ReadData(U16 Addr)
 {
 	U8 Buff;
 	
-	SB_ClrDir();	          //设置SN74LVC16245数据方向设置为输入
+	SB_ClrDir();	          //数据方向设置为输入
 	SB_SetDataRdDir();			//数据读入方向
 	SB_SetAddr(Addr);				//地址输出
 	SB_ClrRd();							//Rd有效
@@ -148,7 +149,7 @@ U8 SB_ReadData(U16 Addr)
 ****************************************************************************************************/
 void SB_WriteData(U16 Addr,U8 Buff)
 {	
-	SB_SetDir();            //设置SN74LVC16245数据方向设置为输出
+	SB_SetDir();            //数据方向设置为输出
 	SB_SetDataWrDir();			//数据输出方向
 	SB_SetAddr(Addr);				//地址输出
 	SB_SetData(Buff);				//数据输出	
@@ -157,9 +158,3 @@ void SB_WriteData(U16 Addr,U8 Buff)
 	__NOP(),__NOP();				//延时
 	SB_SetWr();						  //Wr无效
 }
-/****************************************************************************************************
-以下是应用级驱动函数：基于部分硬件完成应用功能
-****************************************************************************************************/
-/****************************************************************************************************
-以下是硬件级驱动函数：完全基于硬件
-****************************************************************************************************/
